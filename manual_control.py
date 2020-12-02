f70293ce83074ec6e74b3aad01d6808f0c4a4c2d
Tóth Mátyás <62833377+mattyer282@users.noreply.github.com>
2020-12-02 23:56:04 +0100

    sávdetektáló függvények hozzáadása
    Csak a szín szerinti szegmentálást tettem bele. Világos, egyenes szakaszokon egész jól észreveszi a sávokat, de sokszor a kijelölés jobb oldala csak a sáv feléig megy el. Belepakoltam a függvényeket egy LaneDetectorSensor osztályba, amiket a CameraManager _parse_image függvényében hívogatunk meg.

--- manual_control.py
+++ manual_control.py
@@ -877,7 +877,254 @@ class RadarSensor(object):
                 life_time=0.06,
                 persistent_lines=False,
                 color=carla.Color(r, g, b))
+                
+# ==============================================================================
+# -- LaneDetectorSensor --------------------------------------------------------
+# ==============================================================================
 
+class LaneDetectorSensor(object):
+    def __init__(self, parent_actor, hud):
+        self.sensor = None
+        self._parent = parent_actor
+        self.hud = hud
+        world = self._parent.get_world()
+        bp = world.get_blueprint_library().find('sensor.other.lane_invasion')
+        self.sensor = world.spawn_actor(bp, carla.Transform(), attach_to=self._parent)
+        # We need to pass the lambda a weak reference to self to avoid circular
+        # reference.
+        weak_self = weakref.ref(self)
+
+    # Apply color threshold
+    def color_threshold(self, image, thresh=(140, 255)):
+        # Convert to hls
+        image_hls = cv2.cvtColor(image, cv2.COLOR_BGR2HLS)
+        # Get saturation
+        img_sat = image_hls[:,:,1]
+        # Threshold using inRange
+        s_binary = cv2.inRange(img_sat, thresh[0], thresh[1])
+        return s_binary
+
+    def warp(self, img):
+        img_size = (img.shape[1], img.shape[0])
+
+        # Source and destination points
+        src = np.float32(
+              [[380, 0],
+              [875, 235],
+              [60, 235],
+              [470, 0]])
+
+        dst = np.float32(
+             [[150, 0],
+             [800, 260],
+             [150, 260],
+             [800, 0]])
+
+        # Get perspective transforms
+        M = cv2.getPerspectiveTransform(src, dst)
+        Minv = cv2.getPerspectiveTransform(dst, src)
+        # Warp image
+        binary_warped = cv2.warpPerspective(img, M, img_size, flags=cv2.INTER_LINEAR)
+        return binary_warped, Minv
+
+    def get_histogram(self, binary_warped):
+        binary_warped = binary_warped[131:260, 0:930]
+        histogram = np.sum(binary_warped[binary_warped.shape[0] // 2:, :], axis=0)
+        return histogram
+
+    def draw_lane_lines(self, original_image, warped_image, Minv, draw_info):
+        leftx = draw_info['leftx']
+        rightx = draw_info['rightx']
+        left_fitx = draw_info['left_fitx']
+        right_fitx = draw_info['right_fitx']
+        ploty = draw_info['ploty']
+
+        warp_zero = np.zeros_like(warped_image).astype(np.uint8)
+        color_warp = np.dstack((warp_zero, warp_zero, warp_zero))
+
+        pts_left = np.array([np.transpose(np.vstack([left_fitx, ploty]))])
+        pts_right = np.array([np.flipud(np.transpose(np.vstack([right_fitx, ploty])))])
+        pts = np.hstack((pts_left, pts_right))
+
+        cv2.fillPoly(color_warp, np.int_([pts]), (0,255, 0))
+
+        newwarp = cv2.warpPerspective(color_warp, Minv, (original_image.shape[1], original_image.shape[0]))
+        result = cv2.addWeighted(original_image, 1, newwarp, 0.3, 0)
+
+        return result
+
+    def slide_window(self, binary_warped, histogram):
+
+        # Get line bases
+        midpoint = np.int(histogram.shape[0]/2)
+        leftx_base = np.argmax(histogram[:midpoint])
+        rightx_base = np.argmax(histogram[midpoint:]) + midpoint
+
+        # Define sliding window
+        nwindows = 9
+        window_height = np.int(binary_warped.shape[0]/nwindows)
+
+        # Get nonzero pixels
+        nonzero = binary_warped.nonzero()
+        nonzeroy = np.array(nonzero[0])
+        nonzerox = np.array(nonzero[1])
+
+        # Initialize sliding window
+        leftx_current = leftx_base
+        rightx_current = rightx_base
+        margin = 100
+        minpix = 50
+        left_lane_inds = []
+        right_lane_inds = []
+
+        for window in range(nwindows):
+            # Compute window coordinates
+            win_y_low = binary_warped.shape[0] - (window+1)*window_height
+            win_y_high = binary_warped.shape[0] - window*window_height
+            win_xleft_low = leftx_current - margin
+            win_xleft_high = leftx_current + margin
+            win_xright_low = rightx_current - margin
+            win_xright_high = rightx_current + margin
+
+            # Gather points that fit into the window
+            good_left_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) &
+            (nonzerox >= win_xleft_low) &  (nonzerox < win_xleft_high)).nonzero()[0]
+            good_right_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) &
+            (nonzerox >= win_xright_low) &  (nonzerox < win_xright_high)).nonzero()[0]
+
+            # Append to list
+            left_lane_inds.append(good_left_inds)
+            right_lane_inds.append(good_right_inds)
+
+            # If found enough pixels, then shift the line midpoint
+            if len(good_left_inds) > minpix:
+                leftx_current = np.int(np.mean(nonzerox[good_left_inds]))
+            if len(good_right_inds) > minpix:
+                rightx_current = np.int(np.mean(nonzerox[good_right_inds]))
+
+        # Reshape index array
+        left_lane_inds = np.concatenate(left_lane_inds)
+        right_lane_inds = np.concatenate(right_lane_inds)
+
+        # Gather point coordinates
+        leftx = nonzerox[left_lane_inds]
+        lefty = nonzeroy[left_lane_inds]
+        rightx = nonzerox[right_lane_inds]
+        righty = nonzeroy[right_lane_inds]
+
+        # Fit second orde polynoms
+        left_fit = np.polyfit(lefty, leftx, 2)
+        right_fit = np.polyfit(righty, rightx, 2)
+
+        # Get points that fit on the polynoms
+        left_lane_inds = ((nonzerox > (left_fit[0]*(nonzeroy**2) + left_fit[1]*nonzeroy +
+        left_fit[2] - margin)) & (nonzerox < (left_fit[0]*(nonzeroy**2) +
+        left_fit[1]*nonzeroy + left_fit[2] + margin)))
+
+        right_lane_inds = ((nonzerox > (right_fit[0]*(nonzeroy**2) + right_fit[1]*nonzeroy +
+        right_fit[2] - margin)) & (nonzerox < (right_fit[0]*(nonzeroy**2) +
+        right_fit[1]*nonzeroy + right_fit[2] + margin)))
+
+        # Refit polynom
+        leftx = nonzerox[left_lane_inds]
+        lefty = nonzeroy[left_lane_inds]
+        rightx = nonzerox[right_lane_inds]
+        righty = nonzeroy[right_lane_inds]
+        left_fit = np.polyfit(lefty, leftx, 2)
+        right_fit = np.polyfit(righty, rightx, 2)
+
+        # Generate polynom points
+        ploty = np.linspace(0, binary_warped.shape[0]-1, binary_warped.shape[0] )
+        left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
+        right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
+
+        # Return value
+        ret = {}
+        ret['leftx'] = leftx
+        ret['rightx'] = rightx
+        ret['left_fitx'] = left_fitx
+        ret['right_fitx'] = right_fitx
+        ret['ploty'] = ploty
+
+        return ret
+
+    def measure_curvature(self, lines_info):
+        # Approximate meters per pixels
+        ym_per_pix = 30/(260)
+        xm_per_pix = 3.7/(650)
+
+        # Get polynom points
+        leftx = lines_info['left_fitx']
+        rightx = lines_info['right_fitx']
+
+        # Reverse order
+        leftx = leftx[::-1]
+        rightx = rightx[::-1]
+
+        ploty = lines_info['ploty']
+
+        # Compute corvature
+        y_eval = np.max(ploty)
+        left_fit_cr = np.polyfit(ploty*ym_per_pix, leftx*xm_per_pix, 2)
+        right_fit_cr = np.polyfit(ploty*ym_per_pix, rightx*xm_per_pix, 2)
+        left_curverad = ((1 + (2*left_fit_cr[0]*y_eval*ym_per_pix + left_fit_cr[1])**2)**1.5) / np.absolute(2*left_fit_cr[0])
+        right_curverad = ((1 + (2*right_fit_cr[0]*y_eval*ym_per_pix + right_fit_cr[1])**2)**1.5) / np.absolute(2*right_fit_cr[0])
+
+        return left_curverad, right_curverad
+
+    def sanity(self, ret, left_curverad, right_curverad):
+        # Sanity check: whether the lines are roughly parallel and have similar curvature
+        slope_left = ret['left_fitx'][0] - ret['left_fitx'][-1]
+        slope_right = ret['right_fitx'][0] - ret['right_fitx'][-1]
+        slope_diff = abs(slope_left - slope_right)
+        slope_threshold = 150
+        curve_diff = abs(left_curverad - right_curverad)
+        curve_threshold = 10000
+
+        return (slope_diff < slope_threshold and curve_diff < curve_threshold)
+
+
+    def process_image(self, image):
+
+        roi = image[400:710, 220:1150]
+        #print('shape of image', image.shape)
+        #print('shape of roi', roi.shape)
+        # Thresholding
+        binary = self.color_threshold(roi)
+
+        # Transforming Perspective
+        binary_warped, Minv = self.warp(binary)
+
+        # Getting Histogram
+        histogram = self.get_histogram(binary_warped)
+
+        # Sliding Window to detect lane lines
+        ret = self.slide_window(binary_warped, histogram)
+
+        # Measuring Curvature
+        left_curverad, right_curverad = self.measure_curvature(ret)
+
+        # Sanity check
+        #if not self.sanity(ret,left_curverad,right_curverad):
+            # Use last good images if sanity check fails
+            #binary_warped = self.used_warped
+            #ret = self.used_ret
+
+        result = image
+
+        # Visualizing Lane Lines Info
+        result[400:710, 220:1150] = self.draw_lane_lines(roi, binary_warped, Minv, ret)
+
+        # Compute deviation
+        deviation_pixels = image.shape[1]/2 - abs(ret['right_fitx'][-1] - ret['left_fitx'][-1])
+        xm_per_pix = 3.7/(650)
+        deviation = deviation_pixels * xm_per_pix
+
+        # update last good images
+        #self.used_warped = binary_warped
+        #self.used_ret = ret
+
+        return result, deviation, left_curverad
 # ==============================================================================
 # -- CameraManager -------------------------------------------------------------
 # ==============================================================================
@@ -973,32 +1220,35 @@ class CameraManager(object):
             # read image 
             img = cv2.cvtColor(array, cv2.COLOR_BGR2RGB)
             # create framemask
-            stencil = np.zeros_like(img[:,:,0])
-            xl1 = 256
-            xh1 = 630
-            xh2 = 650
-            xl2 = 1024
-            yl = 720
-            yh = 350
-            polygon = np.array([[xl1,yl], [xh1,yh], [xh2,yh], [xl2,yl]])
-            cv2.fillConvexPoly(stencil,polygon,1)
-            masked = cv2.bitwise_and(img[:,:,0], img[:,:,0], mask=stencil)
+            #stencil = np.zeros_like(img[:,:,0])
+            #xl1 = 256
+            #xh1 = 630
+            #xh2 = 650
+            #xl2 = 1024
+            #yl = 720
+            #yh = 350
+            #polygon = np.array([[xl1,yl], [xh1,yh], [xh2,yh], [xl2,yl]])
+            #cv2.fillConvexPoly(stencil,polygon,1)
+            #masked = cv2.bitwise_and(img[:,:,0], img[:,:,0], mask=stencil)
             #plt.figure(figsize=(10,10))
             #plt.imsave('and.jpg', img)
             #exit(1)
 
             # image th + hugh
-            ret, thresh = cv2.threshold(masked, 120,150, cv2.THRESH_BINARY)
+            #ret, thresh = cv2.threshold(masked, 120,150, cv2.THRESH_BINARY)
             # hugh
-            lines = cv2.HoughLinesP(thresh, 1, np.pi/180, 30, maxLineGap = 200)
+            #lines = cv2.HoughLinesP(thresh, 1, np.pi/180, 30, maxLineGap = 200)
+            ld = LaneDetectorSensor(self._parent, self.hud)
+            final_image, dev, curv = ld.process_image(img)
+            
             #towrite 
-            dmy = img.copy()
+            #dmy = img.copy()
             #or with canny 
-            if lines is not None:
-                for line in lines:
-                    x1, y1, x2, y2 = line[0]
-                    cv2.line(dmy, (x1, y1), (x2, y2), (0,250,0), 3)
-            self.surface = pygame.surfarray.make_surface(dmy.swapaxes(0, 1))
+            #if lines is not None:
+            #    for line in lines:
+            #       x1, y1, x2, y2 = line[0]
+            #       cv2.line(dmy, (x1, y1), (x2, y2), (0,250,0), 3)
+            self.surface = pygame.surfarray.make_surface(final_image.swapaxes(0, 1))
 
         if self.recording:
             image.save_to_disk('_out/%08d' % image.frame)
